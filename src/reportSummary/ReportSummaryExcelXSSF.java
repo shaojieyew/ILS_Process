12369120358f6db6a8a94f9f4d3b533314b350e4
YSJ laptop <shaojieyew@gmail.com>
2017-08-18 22:55:50 +0800

    Update bug, ambiguous name

--- src/reportSummary/ReportSummaryExcelXSSF.java
+++ src/reportSummary/ReportSummaryExcelXSSF.java
@@ -37,7 +37,7 @@ public class ReportSummaryExcelXSSF implements ReportSummary {
 	private int recColIndex = -1;
 	private int bandColIndex = -1;
 	private int headerRow = -1;
-	private boolean includeUnknownStudentInCounting = true;
+	private boolean includeUnknownStudentInCounting = false;
 	
 	@Override
 	public void process(ObservableList<Report> reports) {
@@ -108,14 +108,115 @@ public class ReportSummaryExcelXSSF implements ReportSummary {
 				reportList.add(report);
 			}
 		}
+		//boolean[] addedStudentList = new boolean[StudentList.size()];
+		boolean noMoreNewItem=false;
+		do{
+			noMoreNewItem=false;
+			for(int i =0;i<reportList.size();i++){
+				  Report report=reportList.get(i);
+				  List<ExtractedResult> f = FuzzySearch.extractTop(report.getAuthor_name(),StudentList,3);  
+				  boolean ambigouousNameExist = false;
+				  int ambigouousNameCount=0;
+				  int score=0;
+				  for(ExtractedResult x : f){
+					  if(score<x.getScore()){
+						  score=x.getScore();
+					  }else{
+						  if(x.getScore()==score){
+							  ambigouousNameExist =true;
+							  break;
+						  }
+					  }
+					  if(x.getString().toLowerCase().contains(report.getAuthor_name().toLowerCase())){
+						  ambigouousNameCount++;
+						  if(ambigouousNameCount>1){
+							  break;
+						  }
+					  }
+				  }
+				  if(f.size()==0||ambigouousNameCount>1||ambigouousNameExist){
+					  continue;
+				  }
+				  if(report.getAuthor_name().equals(FuzzySearch.extractOne(f.get(0).getString(), NewStudentList).getString())){
+	
+					  //check length
+					  //if(FuzzySearch.tokenSortRatio(x, y.getString())>80)
+					  score = FuzzySearch.tokenSortRatio(f.get(0).getString(), report.getAuthor_name());
+					  //int spaceCount1 =  x.split("\\s+").length -1;
+					  //int spaceCount2 =  y.getString().split("\\s+").length -1;
+					  float xlen = f.get(0).getString().length();
+					  float ylen = report.getAuthor_name().length();
+					  int thresholdScore = 100;
+					  float temp = 1;
+					  if(xlen<ylen){
+						  temp = xlen/ylen;
+					  }
+					  if(xlen>ylen){
+						  temp = ylen/xlen;
+					  }
+					  if(xlen==ylen){
+						  thresholdScore = 85;
+					  }else{
+						  temp=temp-0.45f;
+						  temp = temp*500;
+						  temp = 55+temp/5;
+						  thresholdScore = (int) temp;
+					  }
+					  if(thresholdScore>85){
+						  thresholdScore=85;
+					  }
+					  if(thresholdScore<55){
+						  thresholdScore=55;
+					  }
+					  if(thresholdScore<score){
+						  writeAttributeToList(f.get(0).getString(),report.getAuthor_name(), reportList);
+						  StudentList.remove(f.get(0).getString());
+						  NewStudentList.remove(report.getAuthor_name());
+						  i--;
+						  noMoreNewItem=true;
+					  }
+				  } 
+				  if(reportList.size()==0){
+					  break;
+				  }
+			}
+		}while(noMoreNewItem);
 		
-
+		/*
+		//name matching student
 		for(String x : StudentList){
 			  List<ExtractedResult> f= FuzzySearch.extractSorted(x, NewStudentList,55);
 			  for(ExtractedResult y : f){
 				  if(x.equals(FuzzySearch.extractOne(y.getString(), StudentList).getString())){
+					  //check ambiguousName
+					  List<ExtractedResult> f2 = FuzzySearch.extractAll(x,StudentList,3);
+					  int score = 0;
+					  boolean ambigouousNameExist = false;
+					  int ambigouousNameCount = 0;
+					  for(ExtractedResult x2 : f2){
+						  System.out.println(x2.getString()+" = "+x2.getScore());
+						  if(score<x2.getScore()){
+							  score=x2.getScore();
+						  }else{
+							  if(x2.getScore()==score){
+								  ambigouousNameExist =true;
+								  break;
+							  }
+						  }
+						  if(x2.getString().toLowerCase().contains(y.getString().toLowerCase())){
+							  ambigouousNameCount++;
+						  }
+					  }
+					  if(ambigouousNameCount>1||ambigouousNameExist){
+						  System.out.println("ambigouous Count = "+ambigouousNameCount);
+						  System.out.println("ambigouousNameExist = "+ambigouousNameExist);
+						  break;
+					  }
+						  
+					  
+					  //check length
 					  //if(FuzzySearch.tokenSortRatio(x, y.getString())>80)
-					  int score = FuzzySearch.tokenSortRatio(x, y.getString());
+					  score = FuzzySearch.tokenSortRatio(x, y.getString());
 					  //int spaceCount1 =  x.split("\\s+").length -1;
 					  //int spaceCount2 =  y.getString().split("\\s+").length -1;
 					  float xlen = x.length();
@@ -148,8 +249,9 @@ public class ReportSummaryExcelXSSF implements ReportSummary {
 					  }
 				  }
 			 }
-		}
+		}*/
 		
+		//add not added student
 		//System.out.println(endOfstudentRow);
 		if(endOfstudentRow>-1){
 			cleanSheet(endOfstudentRow,sheet.getLastRowNum(),0,bandColIndex+3);
